b376e27d0f28182269702ab4f83953ba22861c3f
R.O. Ocaya <ocayaro@ufs.ac.za>
2016-05-18 15:49:10 +0200

    Update sim_constants.h

--- sim_constants.h
+++ sim_constants.h
@@ -1,9 +1,9 @@
 // define the simulation constants
-#define eps			1				// an energy parameter
-#define lat_const	3.61					// lattice constant of copper, in angstrom
-#define cn			39.432					// density of state constant
-#define nint		9						// potential function steepness					
-#define mint		6						// potential function range
+#define eps			    1				// an energy parameter
+#define lat_const	  3.61					// lattice constant of copper, in angstrom
+#define cn			    39.432					// density of state constant
+#define nint	    	9						// potential function steepness					
+#define mint		    6						// potential function range
 
 // define macros for index mapping of cells in 3 dimensions
 #define index(ic,nc) ((ic)[0] + (nc)[0]*((ic)[1] + (nc)[1]*(ic)[2]))
